9cb30ca436ee2e5c6c94f1be5c9a1d9ab8b94982
abdullah-mahmoud <abdumah2021fr@gmail.com>
2022-12-22 03:58:22 +0100

    fix js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-frågorBoxGen = [];
-frågorBoxMath = [];
-frågorBoxMed = [];
-
 //category 1 of questions (general questions)
-fråga1 = "Who was the Ancient Greek God of the Sun?"; // appol , hermis, zose
-fråga2 = "What artist has the most streams on Spotify?"; // drake, the weekend, ariana grande
-fråga3 = "What sports car company manufactures the 911?"; // porsche, ferrari, BMW
-fråga4 = "In what country is the Chernobyl nuclear plant located?"; //Ukraine, russia, serbia
+let fråga1 = "Who was the Ancient Greek God of the Sun?"; // appol , hermis, zose
+let fråga2 = "What artist has the most streams on Spotify?"; // drake, the weekend, ariana grande
+let fråga3 = "What sports car company manufactures the 911?"; // porsche, ferrari, BMW
+let fråga4 = "In what country is the Chernobyl nuclear plant located?"; //Ukraine, russia, serbia
 //category 2 of questions (math questions)
-fråga5 = "How many minutes are in a full week?"; // 10,080 , 17,000 , 8960
-fråga6 = "121 Divided by 11 is?"; // 11 , 21 , 12
-fråga7 = "What is the Next Prime Number after 7?"; // 11
+let fråga5 = "How many minutes are in a full week?"; // 10,080 , 17,000 , 8960
+let fråga6 = "121 Divided by 11 is?"; // 11 , 21 , 12
+let fråga7 = "What is the Next Prime Number after 7?"; // 11
 //category 2 of questions (media questions)
-fråga8 = "who is this person?"; // rober downey jr, iron man, your uncel steve
-fråga9 = "whats this rappers name?"; //kendrick lamar, k-dot, kung fu kenny, the goat, all the above
-fråga10 = "what was this movies name?"; //the titanic, titanic, jack and rose story
+let fråga8 = "who is this person?"; // rober downey jr, iron man, your uncel steve
+let fråga9 = "whats this rappers name?"; //kendrick lamar, k-dot, kung fu kenny, the goat, all the above
+let fråga10 = "what was this movies name?"; //the titanic, titanic, jack and rose story
+
+let frågorBoxGen = [fråga1, fråga2, fråga3, fråga4];
+let frågorBoxMath = [fråga5, fråga6, fråga7];
+let frågorBoxMed = [fråga8, fråga9, fråga10];
+
